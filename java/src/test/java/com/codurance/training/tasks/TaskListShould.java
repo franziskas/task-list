23df2fed2d22ac658b771ee5b15d111019be3fef
Franziska Sauerwein <franzi@codurance.com>
2015-07-09 18:53:04 +0100

    refactored test to use Mockito and cleaner setup

--- java/src/test/java/com/codurance/training/tasks/TaskListShould.java
+++ java/src/test/java/com/codurance/training/tasks/TaskListShould.java
@@ -3,55 +3,28 @@ package com.codurance.training.tasks;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Reader;
-import java.io.Writer;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 
 public class TaskListShould {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    private PrintWriter writer = mock(PrintWriter.class);
+    private BufferedReader reader = mock(BufferedReader.class);
+
+    private TaskList taskList = new TaskList(reader, writer);
+
     @Test
     public void
-    should_change_io_exception_in_runtime_exception() {
+    should_change_io_exception_in_runtime_exception() throws Exception {
         final IOException ioException = new IOException();
-        BufferedReader reader = new BufferedReader(new Reader() {
-            @Override
-            public int read(char[] cbuf, int off, int len) throws IOException {
-                return 0;
-            }
-
-            @Override
-            public void close() throws IOException {
-
-            }
-        }) {
-            @Override
-            public String readLine() throws IOException {
-                throw ioException;
-            }
-        };
-        PrintWriter writer = new PrintWriter(new Writer() {
-            @Override
-            public void write(char[] cbuf, int off, int len) throws IOException {
-
-            }
-
-            @Override
-            public void flush() throws IOException {
-
-            }
-
-            @Override
-            public void close() throws IOException {
-
-            }
-        });
-        TaskList taskList = new TaskList(reader, writer);
+        doThrow(ioException).when(reader).readLine();
 
         thrown.expect(RuntimeException.class);
         thrown.expectCause(is(ioException));
